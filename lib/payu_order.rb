e3775648da03ab660a406195ee204f0f000457df
prashant hiremath <prashanthhiremath2@gmail.com>
2018-05-25 18:30:15 +0530

    order changes

--- lib/payu_order.rb
+++ lib/payu_order.rb
@@ -10,35 +10,34 @@ class PayuOrder
       }
     end
 
-    product_name = products.collect{|c| c[:name]}.join(",")
-
-    Rails.logger.info "Order #{order.id}"
-    hash = Digest::SHA512.hexdigest("gtKFFx|#{order.number}|#{(order.total * 100).to_i}|#{product_name}|#{order.bill_address.firstname}|#{order.email}|||||||||||eCwWELxi")
-
     description = I18n.t('order_description',
-                         name: "HandMade")
+                         name: Spree::Store.current.name)
     description = I18n.transliterate(description)
 
     {
-        key: OpenPayU::Configuration.merchant_pos_id,
-        #  customer_ip: ip,
-        txnid: "#{order.number}",
-        amount: (order.total * 100).to_i,
-        #  order_url: order_url,
-        surl: notify_url,
-        furl: continue_url,
-        hash: hash,
-        email: order.email,
-        phone: order.bill_address.phone,
-        productinfo: product_name,
-        firstname: order.bill_address.firstname,
-        salt: "eCwWELxi"
-        # delivery: {
-        #   street: order.shipping_address.address1,
-        #   postal_code: order.shipping_address.zipcode,
-        #   city: order.shipping_address.city,
-        #   country_code: order.bill_address.country.iso
-        # }
+        merchant_pos_id: OpenPayU::Configuration.merchant_pos_id,
+        customer_ip: ip,
+        ext_order_id: order.id,
+        description: description,
+        currency_code: order.currency,
+        total_amount: (order.total * 100).to_i,
+        order_url: order_url,
+        notify_url: notify_url,
+        continue_url: continue_url,
+        buyer: {
+            email: order.email,
+            phone: order.bill_address.phone,
+            first_name: order.bill_address.firstname,
+            last_name: order.bill_address.lastname,
+            language: 'PL',
+            delivery: {
+                street: order.shipping_address.address1,
+                postal_code: order.shipping_address.zipcode,
+                city: order.shipping_address.city,
+                country_code: order.bill_address.country.iso
+            }
+        },
+        products: products
     }
   end
 end
