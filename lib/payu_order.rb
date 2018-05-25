35bf0aee2d4a714dc5b2115c10a3f98e1d4b98f1
prashant hiremath <prashanthhiremath2@gmail.com>
2018-05-25 13:57:46 +0530

    remove items

--- lib/payu_order.rb
+++ lib/payu_order.rb
@@ -29,19 +29,6 @@ class PayuOrder
         order_url: order_url,
         notify_url: notify_url,
         continue_url: continue_url,
-        buyer: {
-            email: order.email,
-            phone: order.bill_address.phone,
-            first_name: order.bill_address.firstname,
-            last_name: order.bill_address.lastname,
-            language: 'PL',
-            delivery: {
-                street: order.shipping_address.address1,
-                postal_code: order.shipping_address.zipcode,
-                city: order.shipping_address.city,
-                country_code: order.bill_address.country.iso
-            }
-        },
         products: products
     }
   end
